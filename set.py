8d819e093e72dc3a8d01761685e756de8469c278
Nitheeshraj99 <162546563+Nitheeshraj99@users.noreply.github.com>
2024-07-23 11:58:50 +0000

    example of sets

---
+++ set.py
@@ -0,0 +1,17 @@
+#my_set = {1, 2, 3, 4, 5}
+
+#my_set.add(6)  # Adding an element
+#my_set.remove(3)  # Removing an element
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+union_set = set1.union(set2)  # Union of sets
+intersection_set = set1.intersection(set2)  # Intersection of sets
+difference_set = set1.difference(set2)  # Difference of sets
+
+print(union_set)
+
+print(intersection_set)
+
+print(difference_set)
