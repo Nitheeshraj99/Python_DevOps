e7e084cb73e04b809521995fc98ede20d645c300
Nitheeshraj99 <162546563+Nitheeshraj99@users.noreply.github.com>
2024-07-23 12:04:11 +0000

    subset and subsetsup

---
+++ subsetsup.py
@@ -0,0 +1,12 @@
+set1 = {1, 2, 3, 4, 5, }
+set2 = {3, 4, 5, 6}
+
+union_set = set1.union(set2)  # Union of sets
+intersection_set = set1.intersection(set2)  # Intersection of sets
+difference_set = set1.difference(set2)  # Difference of sets
+
+is_subset = set1.issubset(set2)  # Checking if set1 is a subset of set2
+is_superset = set1.issuperset(set2)  # Checking if set1 is a superset of set2
+
+print(is_subset)
+print(is_superset)
