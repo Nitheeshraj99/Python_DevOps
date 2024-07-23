c2023721dc09d0e0771ea2cb6db769d344a7f188
Nitheeshraj99 <162546563+Nitheeshraj99@users.noreply.github.com>
2024-07-23 11:35:59 +0000

    dictionaries del add

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+my_dict = {'name': 'John', 'age': 26, 'city': 'New York'}
+#print(my_dict['name']) 
+
+#my_dict['age'] = 26
+
+#my_dict['occupation'] = 'Engineer'
+#print(my_dict)
+
+#del my_dict['city']  # Removing a key-value pair
+#print(my_dict)
+
+#if 'age' in my_dict:
+#   print('Age is present in the dictionary')
+#else:
+#    print('not present')    
