363f3d2e67b096f1114525a2e6738cd699bbbe3b
Nitheeshraj99 <162546563+Nitheeshraj99@users.noreply.github.com>
2024-07-24 16:49:51 +0000

    boto3 project python automation

--- boto3/test.py
+++ boto3/test.py
@@ -1,1 +1,12 @@
+import boto3
 
+client = boto3.client('s3') # is a common syntax  ex: boto3.client('ec2')      ex: boto3.client('ebs')
+
+"""response = client.create_bucket(
+   Bucket='niteesh-boto3',
+)"""
+
+response = client.get_bucket_acl(
+    Bucket='niteesh-boto3',
+)
+print(response)
